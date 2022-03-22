2814c7fd7467ad818b63efc4e1ea9b850fd7a863
Anthony <anthonylaurent.lr@gmail.com>
2022-03-22 14:46:43 +0100

    Create FavoriteRecipeViewController.swift

---
+++ Reciplease1.1/Reciplease1.1/Controller/FavoriteRecipeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteRecipeViewController.swift
+//  Reciplease1.1
+//
+//  Created by Anthony Laurent on 22/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class FavoriteRecipeViewController: UIViewController {
+    
+}
