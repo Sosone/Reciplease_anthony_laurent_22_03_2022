cdbe40010104731c05e92e338a887b1f9bd3eac5
Anthony <anthonylaurent.lr@gmail.com>
2022-04-01 11:30:32 +0200

    Create Pods-Reciplease1.1-Reciplease1.1UITests-dummy.m

---
+++ Reciplease1.1/Pods/Target Support Files/Pods-Reciplease1.1-Reciplease1.1UITests/Pods-Reciplease1.1-Reciplease1.1UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Reciplease1_1_Reciplease1_1UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Reciplease1_1_Reciplease1_1UITests
+@end
