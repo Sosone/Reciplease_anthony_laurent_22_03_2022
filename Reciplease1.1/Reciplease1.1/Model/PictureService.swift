ab967e8ad34b8ec8d6921fe24fc268c7d0c35b75
Anthony <anthonylaurent.lr@gmail.com>
2022-04-11 16:53:34 +0200

    Create PictureService.swift

---
+++ Reciplease1.1/Reciplease1.1/Model/PictureService.swift
@@ -0,0 +1,29 @@
+//
+//  ImageLoadedWithCache.swift
+//  Reciplease1.1
+//
+//  Created by Anthony Laurent on 06/04/2022.
+//
+
+import Foundation
+import Alamofire
+
+//class PictureService {
+//    static var shared = PictureService()
+//    private init() {}
+//    
+//    func maRequete(callBack: @escaping (Bool, Data?) -> Void) {
+//        let monURL = ""
+//        AF.request(monURL).responseData { response in
+//            guard let data = response.result, response.error == nil else {
+//                callBack(false, nil)
+//                return
+//            }
+//            guard let image = response.result.value else {
+//                callBack(false, nil)
+//            }
+//            callBack(true, image)
+//        }
+//    }
+//}
+//
