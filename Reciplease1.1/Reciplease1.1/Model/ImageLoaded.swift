e662c66cfbb8a5f2235a6f0660feb5b9d9fb3165
Anthony <anthonylaurent.lr@gmail.com>
2022-04-11 14:23:46 +0200

    Create ImageLoaded.swift

---
+++ Reciplease1.1/Reciplease1.1/Model/ImageLoaded.swift
@@ -0,0 +1,16 @@
+//
+//  ImageLoadedWithCache.swift
+//  Reciplease1.1
+//
+//  Created by Anthony Laurent on 06/04/2022.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+class ImageLoaded {
+    
+    
+}
+
