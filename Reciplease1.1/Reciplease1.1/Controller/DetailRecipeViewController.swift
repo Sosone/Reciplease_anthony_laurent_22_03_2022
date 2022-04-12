b78e62fecdadd35263633098cd83d5603b46abde
Anthony <anthonylaurent.lr@gmail.com>
2022-04-12 19:01:34 +0200

    Update DetailRecipeViewController.swift

--- Reciplease1.1/Reciplease1.1/Controller/DetailRecipeViewController.swift
+++ Reciplease1.1/Reciplease1.1/Controller/DetailRecipeViewController.swift
@@ -7,34 +7,56 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class DetailRecipeViewController: UIViewController {
     
+    @IBOutlet weak var nameRecipe: UILabel!
     @IBOutlet weak var imageRecipe: UIImageView!
     @IBOutlet weak var ingredientsList: UILabel!
     @IBOutlet weak var cookingTime: UILabel!
     @IBOutlet weak var nbrsOfPeople: UILabel!
+    @IBOutlet weak var buttonFavorite: UIButton!
     
     var recipe: Recipe?
     
-    @IBAction func addFavorite(_ sender: Any) {
-    
+    @objc func onclickAddFavorite(sender:UIButton) {
+        if sender.isSelected == true {
+            sender.isSelected = false
+            print("not Selected")
+        }else {
+            sender.isSelected = true
+            print("Selected")
+
+        }
     }
+    
+    @IBAction func addFavorite(_ sender: UIButton) {
+        buttonFavorite.addTarget(self, action: #selector(self.onclickAddFavorite), for: .touchUpInside)//Target
+        buttonFavorite.setImage(UIImage.init(named: "unfavorite"), for: UIControl.State.normal)//When selected
+        buttonFavorite.setImage(UIImage.init(named: "favorite"), for: UIControl.State.highlighted)//When highlighted
+        buttonFavorite.setImage(UIImage.init(named: "favorite"), for: UIControl.State.selected)//When selected
+        
+    }
+    
     @IBAction func getDirections(_ sender: Any) {
-        let url = NSURL(string: recipe?.urlDescription ?? "http:www.google.com")
+        let url = NSURL(string: recipe!.urlDescription)
         UIApplication.shared.open(url! as URL)
     }
+    
     override func viewDidLoad() {
         cookingTime.text = recipe?.totalTime.description
         ingredientsList.text = recipe?.ingredientLines.joined(separator: "\n")
         nbrsOfPeople.text = recipe?.yield.description
-        
+        nameRecipe.text = recipe?.recipeName.description
+        if let url = URL(string: recipe!.recipeImageURL)
+        ,let data = try? Data(contentsOf: url)
+        {
+            imageRecipe.image = UIImage(data: data)
+        }
     }
     
     
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let url = currentURL?
-//        UIApplication.shared.open(url)
-//    }
+    
 }
 
