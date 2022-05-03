76b92af94274a68c641915c72807b4e6379672e0
Anthony <anthonylaurent.lr@gmail.com>
2022-05-03 11:56:07 +0200

    Update CoreDataStack.swift

--- Reciplease1.1/Reciplease1.1/Model/CoreData/CoreDataStack.swift
+++ Reciplease1.1/Reciplease1.1/Model/CoreData/CoreDataStack.swift
@@ -9,33 +9,26 @@ import Foundation
 import CoreData
 
 final class CoreDataStack {
-    
-    // MARK: - Properties
-
-    private let persistentContainerName = "DataModel"
 
     // MARK: - Singleton
 
-   static let sharedInstance = CoreDataStack()
+    static let sharedInstance = CoreDataStack()
 
     // MARK: - Public
 
     var viewContext: NSManagedObjectContext {
-        return CoreDataStack.sharedInstance.viewContext
+        return CoreDataStack.sharedInstance.persistentContainer.viewContext
     }
 
     // MARK: - Private
 
-    private init() {}
-
     private lazy var persistentContainer: NSPersistentContainer = {
-      let container = NSPersistentContainer(name: persistentContainerName)
-      container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-        if let error = error as NSError? {
-            fatalError("Unresolved error \(error), \(error.userInfo) for \(storeDescription.description)")
-        }
-      })
-      return container
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
     }()
-
 }
