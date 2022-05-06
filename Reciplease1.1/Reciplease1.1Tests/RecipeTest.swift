6146cf43a5bb82d8144a78e682fe14d5160efd09
Anthony <anthonylaurent.lr@gmail.com>
2022-05-06 12:43:36 +0200

    Create RecipeTest.swift

---
+++ Reciplease1.1/Reciplease1.1Tests/RecipeTest.swift
@@ -0,0 +1,28 @@
+//
+//  RecipeTest.swift
+//  Reciplease1.1Tests
+//
+//  Created by Anthony Laurent on 03/05/2022.
+//
+
+import XCTest
+@testable import Reciplease1_1
+
+class RecipeTest: XCTestCase {
+//
+//    func testGivenOneRecipe() {
+//        let recipe = getRecipe()
+//        XCTAssert(recipe.recipeName == "Rillettes")
+//    }
+//    private func getRecipe() -> Recipe {
+//        let name = "Rillettes"
+//        let image = UIImage(named: "recipeDefault") as! Data
+//        let yield = 0
+//        let ingredientLines = ["duck"]
+//        let totalTime = 30
+//        let urlDescription = "wwww.cooking.com"
+//        
+//        let recipe = Recipe(recipeName: name,recipeImage: image, yield: Double(yield), ingredientLines: ingredientLines, totalTime: Double(totalTime), urlDescription: urlDescription)
+//        return recipe
+//    }
+}
