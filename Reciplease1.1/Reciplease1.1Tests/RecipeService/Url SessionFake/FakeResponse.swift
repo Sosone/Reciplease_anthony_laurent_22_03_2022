2918b54f0460122e0d47f377c538a382c6930f03
Anthony <anthonylaurent.lr@gmail.com>
2022-05-06 12:43:28 +0200

    Create FakeResponse.swift

---
+++ Reciplease1.1/Reciplease1.1Tests/RecipeService/Url SessionFake/FakeResponse.swift
@@ -0,0 +1,14 @@
+//
+//  FakeResponse.swift
+//  Reciplease1.1Tests
+//
+//  Created by Anthony Laurent on 02/05/2022.
+//
+
+import Foundation
+
+struct FakeResponse {
+    var response: HTTPURLResponse?
+    var data: Data?
+    var error: Error?
+}
