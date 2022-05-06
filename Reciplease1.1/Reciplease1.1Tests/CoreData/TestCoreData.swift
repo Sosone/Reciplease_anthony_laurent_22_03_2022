81376be38962914a92128bbeda3a10539bccbbfa
Anthony <anthonylaurent.lr@gmail.com>
2022-05-06 12:43:48 +0200

    Create TestCoreData.swift

---
+++ Reciplease1.1/Reciplease1.1Tests/CoreData/TestCoreData.swift
@@ -0,0 +1,16 @@
+//
+//  TestCoreData.swift
+//  Reciplease1.1Tests
+//
+//  Created by Anthony Laurent on 03/05/2022.
+//
+
+import XCTest
+@testable import Reciplease1_1
+
+class TestCoreData: XCTestCase {
+
+    func testSaveRecipe() {
+        
+    }
+}
