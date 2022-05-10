43d331865d92e3e8b000d9a1374a61b31a67a218
Anthony <anthonylaurent.lr@gmail.com>
2022-05-10 18:20:38 +0200

    Update RecipeRepository.swift

--- Reciplease1.1/Reciplease1.1/Model/RecipeRepository.swift
+++ Reciplease1.1/Reciplease1.1/Model/RecipeRepository.swift
@@ -84,47 +84,36 @@ final class RecipeRepository {
         }
     }
     
-//    func delete(recipe: Recipe) {
-//            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
-//            else { return }
-//
-//            let context = appDelegate.persistentContainer.viewContext
-//            let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "RecipeSaved")
-//            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-//
-//            do {
-//                try context.execute(deleteRequest)
-//            } catch let error as NSError {
-//                print(error.localizedDescription)
-//            }
-//        }
+    func deleteAll() {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            else { return }
+
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "RecipeSaved")
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+
+            do {
+                try context.execute(deleteRequest)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
     
-//    func remove(nameSaved: String) {
-//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
-//        else { return }
-//        let context = appDelegate.persistentContainer.viewContext
-//        let request: NSFetchRequest<RecipeSaved> = RecipeSaved.fetchRequest()
-//        let recipe: NSManagedObject = RecipeSaved.init(entity: RecipeSaved.entity(), insertInto: CoreDataStack.sharedInstance.viewContext)
-//        request.predicate = NSPredicate(format: "nameSaved == %@", nameSaved)
-//        let recipes = try? CoreDataStack.sharedInstance.viewContext.fetch(request)
-//        context.delete(recipe)
-//        try? context.save()
-//        return
-//    }
-    
-//    func remove2() {
-//        let context = CoreDataStack.sharedInstance.viewContext
-//        let entity = NSEntityDescription.entity(forEntityName: "RecipeSaved", in: context)!
-//        
-//        context.delete(recipe)
-//        do {
-//            try context.save()
-//            print("La recette est supprimée")
-//        }
-//        catch let error as NSError {
-//            print("Il y a une erreur lors de la suppression: \(error)")
-//        }
-//    }
+    func remove(nameSaved: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        else { return }
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "RecipeSaved")
+        fetchRequest.predicate = NSPredicate(format: "nameSaved == %@", nameSaved)
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        do {
+            try context.execute(deleteRequest)
+            print("La recette est supprimée")
+        } catch let error as NSError {
+            print("Il y a une erreur lors de la suppression: \(error)")
+        }
+    }
     
     
         func checkIfRecipeIsAlreadySaved(name: String) -> Bool {
