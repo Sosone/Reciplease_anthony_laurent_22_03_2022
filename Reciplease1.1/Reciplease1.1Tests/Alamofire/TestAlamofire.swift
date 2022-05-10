21cdb30fa31d2c462fe68f0114131b6bc7eca34b
Anthony <anthonylaurent.lr@gmail.com>
2022-05-10 18:20:27 +0200

    Update TestAlamofire.swift

--- Reciplease1.1/Reciplease1.1Tests/Alamofire/TestAlamofire.swift
+++ Reciplease1.1/Reciplease1.1Tests/Alamofire/TestAlamofire.swift
@@ -17,73 +17,79 @@ class TestAlamofire: XCTestCase {
 //    override class func tearDown() {
 //        <#code#>
 //    }
-    func test() {
-        let expectation = expectation(description: "Wait")
-        RecipeService.shared.getRecipe(ingredients: ["duck"]) { (success, Recipe) in
-            let recipes: [Recipe] = []
-            XCTAssertNotNil(recipes.count)
+    
+    let session: Alamofire.Session = {
+        let configuration = URLSessionConfiguration.af.ephemeral
+        configuration.protocolClasses = [FakeProtocol.self] + (configuration.protocolClasses ?? [])
+        return Alamofire.Session(configuration: configuration)
+    }()
+    
+    
+    func testGetRecipe() {
+
+        FakeProtocol.loadData = {
+            return FakeResponseData.recipeCorrectData
+        }
+
+        let recipeService = RecipeService(session: session, imageLoader: { _ in return Data() })
+
+        let expectation = expectation(description: "wait")
+        recipeService.getRecipe(ingredients: ["duck"]) { (success, recipes) in
             XCTAssertTrue(success)
+            XCTAssertNotNil(recipes)
+            XCTAssertNotEqual(recipes!.count, 0)
             expectation.fulfill()
         }
-        waitForExpectations(timeout: 15) { error in
-            if let error = error {
-                print(error.localizedDescription)
+        
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testCannotGetRecipe()
+    {
+        FakeProtocol.loadData = {
+            return FakeResponseData.recipeIncorrectData
+        }
+
+        let recipeService = RecipeService(session: session, imageLoader: { _ in return Data() })
+
+        let expectation = expectation(description: "wait")
+        recipeService.getRecipe(ingredients: ["duck"]) { (success, recipes) in
+            XCTAssertFalse(success)
+            XCTAssertNil(recipes)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10)
+    }
+}
+    
+    class FakeProtocol: URLProtocol {
+        
+        static var loadData: () -> Data? = { return nil }
+        static var loadError: () -> Error? = { return nil }
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+        
+        override func startLoading() {
+            if let data = Self.loadData()
+            {
+                client?.urlProtocol(self, didReceive: HTTPURLResponse(url: URL(string: "https://google.fr")!, statusCode: 200, httpVersion: nil, headerFields: nil)!, cacheStoragePolicy: .notAllowed)
+                client?.urlProtocol(self, didLoad: data)
+                client?.urlProtocolDidFinishLoading(self)
+            }
+            else if let error = Self.loadError()
+            {
+                client?.urlProtocol(self, didFailWithError: error)
             }
         }
-    }
-    
-//    func testGetRecipe() {
-//
-//        FakeProtocol.loadData = {
-//            return FakeResponseData.recipeCorrectData
-//        }
-//
-//        let configuration = URLSessionConfiguration.af.ephemeral
-//        configuration.protocolClasses = [FakeProtocol.self] + (configuration.protocolClasses ?? [])
-//        let session = Alamofire.Session(configuration: configuration)
-//        let recipeService = RecipeService(session: session)
-//
-//        let expectation = expectation(description: "Wait")
-//        recipeService.getRecipe(ingredients: ["duck"]) { (success,Recipe) in
-//            let recipes: [Recipe] = []
-//            XCTAssertTrue(success)
-//            XCTAssertNotNil(recipes.count)
-//            expectation.fulfill()
-//        }
-//        waitForExpectations(timeout: 10, handler: nil)
-//
-//
-//        }
-    }
-
-
-class FakeProtocol: URLProtocol
-{
-    static var loadData: () -> Data? = { return nil }
-    static var loadError: () -> Error? = { return nil }
-    
-    override class func canInit(with request: URLRequest) -> Bool {
-        return true
-    }
-    
-    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-    }
-    
-    override func startLoading() {
-        if let data = Self.loadData()
-        {
-            client?.urlProtocol(self, didReceive: HTTPURLResponse(url: URL(string: "https://google.fr")!, statusCode: 200, httpVersion: nil, headerFields: nil)!, cacheStoragePolicy: .notAllowed)
-            client?.urlProtocol(self, didLoad: data)
-            client?.urlProtocolDidFinishLoading(self)
-        }
-        else if let error = Self.loadError()
-        {
-            client?.urlProtocol(self, didFailWithError: error)
-        }
-    }
-    
-    override func stopLoading() {
         
-    }
+        override func stopLoading() {
+            
+        }
 }
