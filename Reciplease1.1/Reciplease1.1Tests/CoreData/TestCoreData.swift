27aa9e5acfb07f5cabcdcd03d8fc4ebd3d27fd40
Anthony <anthonylaurent.lr@gmail.com>
2022-05-12 15:32:55 +0200

    Update TestCoreData.swift

--- Reciplease1.1/Reciplease1.1Tests/CoreData/TestCoreData.swift
+++ Reciplease1.1/Reciplease1.1Tests/CoreData/TestCoreData.swift
@@ -33,6 +33,7 @@ class TestCoreData: XCTestCase {
         let savedRecipe = savedRecipes.first(where: { $0.recipeName == "Duck" })
         
         XCTAssertNotNil(savedRecipe)
+        RecipeRepository.shared.remove(nameSaved: "Duck")
     }
     
     func testShouldDeleteSpecifiedRecipe() {
@@ -50,6 +51,29 @@ class TestCoreData: XCTestCase {
         RecipeRepository.shared.remove(nameSaved: "Duck")
         
         let savedRecipes2 = RecipeRepository.shared.retrieve()
+            
         XCTAssertEqual(savedRecipes2.count, 0)
     }
+    
+    func testRecipeIsAlreadySaved() {
+        let image: UIImage = UIImage(named: "recipeDefault")!
+        let data: Data = image.jpegData(compressionQuality: 0)!
+       
+        let recipe: Recipe = Recipe.init(recipeName: "Duck", recipeImage: data, yield: 4, ingredientLines: ["duck,orange"], totalTime: 120, urlDescription: "www.blablabla.fr")
+        RecipeRepository.shared.save(recipe: recipe)
+        
+        let savedRecipes = RecipeRepository.shared.retrieve()
+        let savedRecipe = savedRecipes.first(where: { $0.recipeName == "Duck" })
+        XCTAssertNotNil(savedRecipe)
+        
+        let favRecipe = RecipeRepository.shared.checkIfRecipeIsAlreadySaved(name: "Duck")
+        XCTAssertEqual(true, favRecipe)
+        RecipeRepository.shared.remove(nameSaved: "Duck")
+    }
+    
+    func testRecipeIsNoFavorite() {
+        let favRecipe = RecipeRepository.shared.checkIfRecipeIsAlreadySaved(name: "Duck")
+        XCTAssertEqual(false, favRecipe)
+    }
+    
 }
